2039ae5f9930c85367e71e5005d66db88582c0ab
Franekdowokdwmdseofklds <franekbananek3@gmail.com>
2025-05-05 18:15:08 +0200

    Player movement finished

--- Labyrinth Teleport Game/Assets/Scripts/CameraController.cs
+++ Labyrinth Teleport Game/Assets/Scripts/CameraController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float sensivity = 100;
+    [SerializeField] private float mouseSensivity = 100;
 
     private Transform playerBody;
-    private float xRotation = 0;
+    private float xRotation = 0.0f;
 
     void Start()
     {
@@ -16,6 +16,18 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 80);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
